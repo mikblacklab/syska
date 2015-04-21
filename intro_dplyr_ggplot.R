425b09461e925899bd44d2eb8754c3420e1e45ff
Murray Cadzow <murray.cadzow@otago.ac.nz>
2015-04-22 09:40:31 +1200

    added extra comments to explain further

--- intro_dplyr_ggplot.R
+++ intro_dplyr_ggplot.R
@@ -6,11 +6,13 @@ library(dplyr)
 library(ggplot2)
 
 
-#data is stored on the Xsan in staff_groups/blacklab/Documents/syska_data/
+
 #data is an reduced sample of iHS scores by position, annotated with rsIds and gene names
+#data is stored on the Xsan in staff_groups/blacklab/Documents/syska_data/
 load("ann.Rdata")
 
 #arrange dataset based on |iHS| values (ascending order)
+# %>% should be read as 'then do', it is also know as a pipe
 ann1 = ann %>% arrange(abs(iHS))
 
 #arrange dataset in descending order based on
@@ -23,10 +25,36 @@ ann1$rank = as.numeric(rownames(ann1))
 ann1=na.omit(ann1)
 
 genes=c("ABCG2", "SLC2A9", "PPARGC1A")
-ann1 %>% filter(gene %in% genes) %>% group_by(gene) %>% summarise(min(rank), quantile(rank, 0.25),median(rank), mean(rank),quantile(rank, 0.75), max(rank))
-a<- ann1 %>%  group_by(gene) %>% summarise(min_rank=min(rank), firstQu=quantile(rank, 0.25),med=median(rank), me=mean(rank), thirdQu=quantile(rank, 0.75), max_rank=max(rank), len=length(rank), v=var(rank)) %>% arrange(me, v)
+
+#for the genes we want, group the ranks by gene and then calculate various statistics about the rank
+ann1 %>% filter(gene %in% genes) %>%
+  group_by(gene) %>%
+  summarise(min(rank),
+            quantile(rank, 0.25),median(rank),
+            mean(rank),
+            quantile(rank, 0.75),
+            max(rank)
+            )
+
+#take data and for each gene report on rank using a custom version of summary
+a<- ann1 %>%
+  group_by(gene) %>%
+  summarise(min_rank=min(rank),
+            firstQu=quantile(rank, 0.25),
+            med=median(rank), me=mean(rank),
+            thirdQu=quantile(rank, 0.75),
+            max_rank=max(rank),
+            len=length(rank), #how many snps in the gene
+            v=var(rank)) %>%
+  arrange(me, v)
+
+#look at top genes with lowest mean for rank
 head(a)
 
-#create boxplot for rank distribution of selected genes
-ann1 %>% filter(gene %in% genes) %>% group_by(gene) %>% ggplot(. , aes(x=gene, y=rank)) + geom_boxplot() +theme_bw()
+#filter for genes we want then create boxplot to look at distribution of rank in those genes
+# the '.' in ggplot(. ,aes()) means take the incoming data from the pipe
+ann1 %>%
+  filter(gene %in% genes) %>%
+  group_by(gene) %>%
+  ggplot(. , aes(x=gene, y=rank)) + geom_boxplot() +theme_bw()
 
